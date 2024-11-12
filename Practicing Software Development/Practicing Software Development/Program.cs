ee1d7347fee74b2e850f5c15dcca367a5acee79e
StrawberrySnakes <dessashapiro@gmail.com>
2024-11-12 12:47:51 -0500

    added starter code

---
+++ Practicing Software Development/Practicing Software Development/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practicing_Software_Development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
