2f78b0d178e3fcacf5ac143257b4018aab0b7c22
Ethan-Shek <dinoeshek@gmail.com>
2024-11-12 21:28:42 -0500

    Ethans part
    I finished my part. it should work let me know if there is anything else

--- Practicing Software Development/Program.cs
+++ Practicing Software Development/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ethan's part
+            // This dictionary is just a temoporary one I use this one to make sure my method works
+            //The dictionary NEEDS TO BE STRING
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>
+        {
+            {"bob", "1231"},
+            {"charlie", "1232"},
+            {"alvin", "1233"}
+        };
+
+
+            // Sort dictionary alphabetically by keys
+            var sortedPhoneBook = phoneBook.OrderBy(pair => pair.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            foreach (KeyValuePair<string, string> pair in sortedPhoneBook)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
         }
     }
 }
