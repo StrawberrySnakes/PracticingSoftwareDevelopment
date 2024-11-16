6438a17a4512e5b7a62eae40dc69e0db2bbbe4f5
StrawberrySnakes <dessashapiro@gmail.com>
2024-11-16 14:55:57 -0500

    some spelling errors and other small changed

--- Practicing Software Development/Program.cs
+++ Practicing Software Development/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System.Linq;
+using System.Collections.Generic;
 namespace Practicing_Software_Development
 {
     internal class Program
@@ -6,7 +7,9 @@ namespace Practicing_Software_Development
         // pre-made Dictionary phoneBook with 3 entries Ethan with a Small upodate by Jayson
        static Dictionary<string, string> phoneBook = new Dictionary<string, string>
         {
-            {"Alvin", "555-1233"}, {"Brittany", "555-4567"}, {"Simon", "555-8910"}
+            {"Alvin", "555-1233"},
+            {"Brittany", "555-4567"},
+            {"Simon", "555-8910"}
         };
         static void Main(string[] args)
         {
@@ -43,7 +46,8 @@ namespace Practicing_Software_Development
                     RemoveEntry();
                     break;
                 case "5":
-                    Console.WriteLine("Closing the phonebook, bye!");
+                    Console.WriteLine("\nClosing the phonebook, bye!");
+                    //exit program
                     return;
 
                 default://  If they don't choose from one of the given options correctly remind them too and call the MainMenu Function Again
@@ -72,7 +76,7 @@ namespace Practicing_Software_Development
         // Function that adds new entries to the phonebook
         static void AddEntry()
         {
-            Console.WriteLine("\nEnter the nane: ");
+            Console.WriteLine("\nEnter the name: ");
             string name = Console.ReadLine();
             Console.WriteLine("\nEnter the phone number: ");
             string phoneNumber = Console.ReadLine();
@@ -90,7 +94,7 @@ namespace Practicing_Software_Development
         // Jayson's function that removes entries
         static void RemoveEntry()
         {
-            Console.WriteLine("\nEnter the nane: ");
+            Console.WriteLine("\nEnter the name: ");
             string key = Console.ReadLine();
             if (!phoneBook.ContainsKey(key))
             {
@@ -107,11 +111,11 @@ namespace Practicing_Software_Development
         //Dessa's functions that seaches using name
         static void SearchEntry()
         {
-            Console.WriteLine("\nEnter the nane: ");
+            Console.WriteLine("\nEnter the name: ");
             string keyToFind = Console.ReadLine();
             if (!phoneBook.ContainsKey(keyToFind))
             {
-                Console.WriteLine($"{keyToFind} doens't exixst in the phonebook");
+                Console.WriteLine($"{keyToFind} doesn't exixst in the phonebook");
             }
             else
             {
